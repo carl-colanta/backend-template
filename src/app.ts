056edfaa35410461a17a7a5bf2d57a7276a29615
Carl <carl.colanta>
2026-07-23 05:57:41 -0700

    clean ups

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
+import Fastify from "fastify";
+import { env } from "./config/index.js";
 
-export class Application {
-    async initialize() {
-        console.log("Application has started");
-    }
+export function createApp() {
+
+    const app = Fastify({
+        logger: true
+    });
+
+    app.get("/", async () => {
+        return {
+            message: "Response mock"
+        };
+
+    });
+
+    return app;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
-import { Application } from "./app.ts";
+import { createApp } from "./app.js";
+import { env } from "./config/index.js";
 
+
+export async function startServer() {
+
+    const app = createApp();
+
+    await app.listen({
+        port: env.PORT,
+        host: "localhost"
+    });
+
+    console.log(
+        `Server running on port ${env.PORT}`
+    );
+}
