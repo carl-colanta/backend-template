fe2ecdff8aa7c824a5fdfb154edadeb2fe69b69c
Carl <carl.colanta>
2026-07-26 20:24:34 -0700

    clean up

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,24 +1,24 @@
 import type { FastifyInstance } from "fastify";
-import type { FastifyRequest, FastifyReply } from 'fastify';
+import type { FastifyRequest, FastifyReply } from "fastify";
 
 // POC libarary
 // This can be updated later on or write a custom library that's faster through C++, or anything with less abstraction
 import screenshot from "screenshot-desktop";
 
 export async function routes(app: FastifyInstance) {
-   app.get("/screen",  async (request: FastifyRequest, reply: FastifyReply) => {
+  app.get("/screen", async (request: FastifyRequest, reply: FastifyReply) => {
     const imgPath: String = `./tmp/demo-${Date.now()}.png`;
     // Initial POC just to see how screenshots work
     // Add fp either
     try {
-       screenshot({ filename: imgPath });
-       return {
-           message: "screenshot " + imgPath
-       }
+      screenshot({ filename: imgPath });
+      return {
+        message: "screenshot " + imgPath,
+      };
     } catch (error) {
       console.error("Error capturing screen: ", error);
     }
 
-    return reply.code(201).send({ status: 'Created', path: imgPath });    
+    return reply.code(201).send({ status: "Created", path: imgPath });
   });
 }
